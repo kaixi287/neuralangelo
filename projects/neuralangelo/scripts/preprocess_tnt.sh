ca5eb547ca651321b7b6b5e0697f854725a2d585
kaixi <kaixi-bao>
2023-12-04 23:39:45 +0100

    Adapt preprocess Ignatius

--- projects/neuralangelo/scripts/preprocess_tnt.sh
+++ projects/neuralangelo/scripts/preprocess_tnt.sh
@@ -10,11 +10,5 @@
 
 # usage: tnt_download.sh <path_to_tnt>
 
-echo "Download fixed poses for Ignatius"
-gdown 10pcCwaQY6hqyiegJGdgmLp_HMFOnsmgq
-gdown 19TT5aTz3z60eUVIDhFJ7EgGqpcqQnJEi
-mv Ignatius_COLMAP_SfM.log ${1}/Ignatius/Ignatius_COLMAP_SfM.log
-mv Ignatius_trans.txt ${1}/Ignatius/Ignatius_trans.txt
-
 echo "Compute intrinsics, undistort images and generate json files. This may take a while"
 python3 projects/neuralangelo/scripts/convert_tnt_to_json.py --tnt_path "/content/drive/MyDrive/neuralangelo_datasets/tanks_and_temples"
